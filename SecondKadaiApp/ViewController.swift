e7cd323597d5acdbe08b9d080f5180c65657a8d0
megumi.K <daron.d.libra@gmail.com>
2021-01-18 20:32:43 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 片桐めぐみ on 2021/01/18.
+//  Copyright © 2021 megumi.katagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
